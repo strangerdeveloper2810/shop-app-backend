cc5b6191f800c74058e6649a4af8c572d43882b1
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2023-10-13 02:09:20 +0700

    write request get, post, put, delete basic in spring boot

---
+++ src/test/java/com/shopapp/shopapp/ShopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopapp.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
