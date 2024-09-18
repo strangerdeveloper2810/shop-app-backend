e54691876674c5d0445255a216a46d7bee624382
Trình Nguyễn <nguyenmaster3012@gmail.com>
2024-09-19 01:30:38 +0700

    resolved

--- src/main/java/com/shopapp/shopapp/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopapp.shopapp.repositories;
-
-import com.shopapp.shopapp.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/shopapp/shopapp/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopapp.shopapp.repositories;
-
-import com.shopapp.shopapp.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
